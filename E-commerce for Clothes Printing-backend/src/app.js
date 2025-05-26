d404210b01d177f2591b38c1003a7fef9f0004c7
yuvraj singh shori <yuvrajshori5@gmail.com>
2025-05-27 01:51:37 +0530

    added frontend live link in backend

--- E-commerce for Clothes Printing-backend/src/app.js
+++ E-commerce for Clothes Printing-backend/src/app.js
@@ -11,7 +11,7 @@ import paymentRoutes from "./routes/payment.route.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://designdrip-v1.netlify.app/",
     credentials: true,
   })
 );
