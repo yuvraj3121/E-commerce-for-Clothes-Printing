14e81aceb7ac8454cd595f6c955dd3296d94c70a
yuvraj singh shori <yuvrajshori5@gmail.com>
2025-05-29 18:45:45 +0530

    updates in origin

--- E-commerce for Clothes Printing-backend/src/app.js
+++ E-commerce for Clothes Printing-backend/src/app.js
@@ -11,7 +11,7 @@ import paymentRoutes from "./routes/payment.route.js";
 const app = express();
 app.use(
   cors({
-    origin: "https://designdrip-v1.netlify.app",
+    origin: ["https://designdrip-v1.netlify.app", "http://localhost:5173"],
     credentials: true,
   })
 );
